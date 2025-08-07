37e7b3a5cab5698f6a73cf876151239ece1e7aa9
reena-bohara <reenabohara.m@gmail.com>
2025-08-07 13:56:15 +0530

    LinkedList Node

---
+++ LinkedList_Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+# Example
+#node1 = Node(5)
+#node2 = Node(10)
+#node1.next = node2
+#print(node1.data, '->', node1.next.data)
